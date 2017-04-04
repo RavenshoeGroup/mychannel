10e87dea28af26d0b0f7cb2fed15520126eb4498
smazing <smazingwebdev@gmail.com>
2017-04-04 13:55:25 -0400

    Add files via upload

---
+++ public/inc/classes.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('classes/mysql.class.php');
+require_once('classes/helper.class.php');
+require_once('classes/user_class.php');
+require_once('classes/user_row_class.php');
+
+?>
---
+++ public/index.php
@@ -0,0 +1,264 @@
+<!doctype html>
+<html> 
+<meta http-equiv="refresh" content="1800">
+<!-- InstanceBegin template="/Templates/template.dwt" codeOutsideHTMLIsLocked="false" --><head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="_images/favicon.ico" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    
+
+	<!-- InstanceBeginEditable name="metadescription" -->
+    <meta property="og:description" name="Description" content="Growing your business through innovative marketing solutions. Complete in-house marketing services. The original hybrid agency."/>
+    <meta property="og:url" content="http://www.ravenshoegroup.com/" />
+    <meta property="og:title" content="Full Service Marketing | Web, Retail, Print | Ravenshoe Group" /> 
+    <meta property="og:type" content="website" /> 
+    <!-- InstanceEndEditable -->
+
+	<!-- InstanceBeginEditable name="doctitle" -->
+      <title>RSGTV® | Come in and have a coffee with us</title>
+    <!-- InstanceEndEditable -->
+
+	<!-- Bootstrap Framework-->
+    <!--<link href="../_css/bootstrap.css" rel="stylesheet">-->
+    <link href="_css/bootstrap.min.css" rel="stylesheet">
+    <link href="_css/theme.css" rel="stylesheet">
+    <link href="_css/font-awesome.min.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="_js/plugins/html5shiv.js"></script>
+      <script src="_js/plugins/respond.min.js"></script>
+    <![endif]-->
+    
+	<!-- InstanceBeginEditable name="head" -->
+  <!-- <link type='text/css' rel='stylesheet' href='style/reset.css' />
+  <link type='text/css' rel='stylesheet' href='style/game.css' /> -->
+  <script src="https://cdn.socket.io/socket.io-1.0.0.js"></script>
+  <script src='_js/lib/phaser.min.js'></script>
+  <script src="_js/extras/phaser-virtual-joystick.min.js" type="text/javascript"></script>
+  <!-- InstanceEndEditable -->
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300italic,300,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+
+
+<style type="text/css">
+
+@media (max-width: 480px) {
+  .default-wrapper{display:none!important;}
+  #wrapper{background-color: black!important;}
+  body{background-color: black!important;}
+}
+
+</style>
+
+</head>
+
+<body>
+<div id="wrapper">
+
+
+<!-- Header -->
+
+<!-- header end -->
+
+
+
+<!-- InstanceBeginEditable name="mainContent" -->
+
+<!--<div class="default-wrapper">
+  <div class="container-fluid">
+	<div class="row">
+         
+      <div class="col-sm-12">
+      <div class="col-sm-12" style="background-color:#222222; height:10vh;"></div>
+      
+      <div class="col-sm-9" style="height:90vh;">
+        <div class="col-sm-12" style="background-color:#F5F5F5; height:60vh;"></div>
+        <div class="col-sm-4" style="background-color:#8a8a8a; height:30vh;"></div>        
+        <div class="col-sm-8" style="height:30vh; padding:2% 40px 0px 40px;"><iframe id="forecast_embed" type="text/html" frameborder="0" height="97%" width="100%" src="http://forecast.io/embed/#lat=43.843090&lon=-79.308251&name=Markham&units=uk"></iframe></div>    
+      </div>
+      
+      <div class="col-sm-3" style="background-color:#505050; height:90vh;"><div id="viewport"></div></div>
+      
+      
+      </div>
+   
+	</div>
+  </div>
+</div>-->
+
+<div class="default-wrapper" style="position:relative;">
+  <div class="container-fluid">
+	<div class="row">
+
+<!--CONTAINER -->   
+<div class="col-md-16" style="height:100%; position:absolute;">
+      
+          
+<!--HEADER -->         
+          <div id="hello" class="col-md-16 sports-bg" style="height:10%; position:relative;">
+          
+          <div id="mini">
+		  <!--  <?php
+    
+           // $output = '';
+           // // URL location of your feed
+           // $feedUrl = "http://www.ravenshoegroup.com/RSGTV/sports/all?feed=rss2";
+           // $feedContent = "";
+            
+           // // Fetch feed from URL
+           // $curl = curl_init();
+           // curl_setopt($curl, CURLOPT_URL, $feedUrl);
+           // curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+           // curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+           // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+           // curl_setopt($curl, CURLOPT_HEADER, false);
+            
+           // // FeedBurner requires a proper USER-AGENT...
+           // curl_setopt($curl, CURL_HTTP_VERSION_1_1, true);
+           // curl_setopt($curl, CURLOPT_ENCODING, "gzip, deflate");
+           // curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3");
+            
+           // $feedContent = curl_exec($curl);
+           // curl_close($curl);
+            
+           // // Did we get feed content?
+           // if($feedContent && !empty($feedContent)){
+           // $feedXml = @simplexml_load_string($feedContent);
+           // $count = 0;
+           //  if($feedXml){ 
+           //    foreach($feedXml->channel->sport as $sport){
+           //      $output .= '<strong><span style="color:#FAFAFA; text-decoration:underline; font-size:16px;">'.$sport->title.'</span></strong>';
+           //      ?>
+           //      <?php
+           //      foreach($sport as $item){
+           //        $output .= '<span style="color:#FAFAFA; font-size:14px;">'.$item->title.'</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+           ?>       
+           <?php //}}}} ?> -->
+    
+            <div class="ticker"
+                 data-duration="100"
+                 data-loop="1"
+                 data-effect="linear"
+            >
+              <span><!-- <?php //echo $output; ?> --></span>
+            </div>
+        </div>
+          
+          </div>
+          
+          
+<!--BODY -->          
+          <div class="col-md-16" style="height:83%; position:relative;">
+            
+             
+             <div class="col-md-6" style="height:100%; position:relative;">   
+             
+               <div class="col-md-16 text-center" style="background-color:#1c1c1c; height:30%; position:relative;">
+               <div class="col-md-16" style="height:75%;"> <br>RSG LOGO </div>
+               <div class="col-md-16" style="height:25%;"><p class="date"><?php echo date("F j, Y  g:i a");?></p></div>
+               
+               </div>
+               <div class="col-md-16" style="background-color:rgba(125,125,125,1.00); height:70%; position:relative; background-image:url(_images/what-we-do-bg.jpg); background-size:cover; background-position:top left;">
+               
+               <!--<div id="coffee"><h5 class="white"><em>"Come in and have<br>a coffee with us."</em></h5></div>-->
+               <div id="viewport"></div> 
+               </div>
+            
+             </div>
+          
+             
+             <div class="col-md-10" style="height:100%; position:relative;">
+             
+               <div class="col-md-16" style="background-color:#FFFFFF; position:relative;">
+              
+               <div class="fullscreen-bg">
+                    <video loop muted autoplay poster="rsgvideo.jpg" class="fullscreen-bg__video">
+                        <source src="rsgvideo.mp4" type="video/mp4">
+                    </video>
+                </div>
+                               
+               <div class="col-md-16 text-center" style="height:100%; overflow:visible;">
+               
+               <iframe id="forecast_embed" class="fe_container" type="text/html" frameborder="0" height="100%" width="100%" src="http://forecast.io/embed/#lat=43.843090&lon=-79.308251&name=Markham&units=uk"></iframe>
+               </div> 
+               
+               </div>
+               
+            
+             </div>
+               
+                         
+             
+                
+          </div>
+
+
+
+<!--FOOTER -->
+		  <div class="col-md-16 text-center" style="background-color:#1c1c1c; min-height:7%; position:relative; bottom:0px;"><p style="color:#C4C4C4; padding-top:10px;">© <?php echo date("Y");?> Ravenshoe Group.</p></div>
+
+
+</div>
+
+
+    </div>
+  </div>
+</div>
+
+
+
+<!-- InstanceEndEditable -->
+
+
+
+<!-- Footer -->
+
+<!-- footer end -->
+
+<div id="play"></div>
+</div><!-- wrapper end -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+
+<!-- InstanceBeginEditable name="footer" -->
+<script src="_js/extras/LocalPlayer.js"></script>
+<script src="_js/extras/boot.js"></script>
+<script src="_js/extras/load.js"></script>
+<script src="_js/extras/menu.js"></script>
+<script src="_js/extras/play.js"></script>
+<script src='_js/extras/game.js'></script>
+
+
+<script src="_js/lib/jquery.min.js"></script>
+<script src="_js/plugins/jquery.jticker.js"></script>
+<script src="_js/plugins/essentials.js"></script>
+<script src="_js/framework/bootstrap.min.js"></script>
+
+<script>
+$('.ticker').jTicker();
+</script>
+
+<script src="jquery-1.js"></script>
+<script type="text/javascript">
+
+jQuery.noConflict();
+(function( $ ) {
+  $(function(){
+    if(!$.browser.msie){  var a=0;for(;a<10;a+=1){setTimeout(function b(){var a=Math.random()*1e3+5e3,c=$("<div />",{"class":"smoke",css:{opacity:0,left:Math.random()*90+0}});$(c).appendTo("#viewport");$.when($(c).animate({opacity:1},{duration:a/4,easing:"linear",queue:false,complete:function(){$(c).animate({opacity:0},{duration:a/3,easing:"linear",queue:false})}}),$(c).animate({bottom:$("#viewport").height()},{duration:a,easing:"linear",queue:false})).then(function(){$(c).remove();b()})},Math.random()*3e3)}
+    }else{    
+       "use strict";var a=0;for(;a<15;a+=1){setTimeout(function b(){var a=Math.random()*1e3+5e3,c=$("<div />",{"class":"smoke",css:{left:Math.random()*90+0}});$(c).appendTo("#viewport");$.when($(c).animate({},{duration:a/4,easing:"linear",queue:false,complete:function(){$(c).animate({},{duration:a/3,easing:"linear",queue:false})}}),$(c).animate({bottom:$("#viewport").height()},{duration:a,easing:"linear",queue:false})).then(function(){$(c).remove();b()})},Math.random()*3e3)}
+    }
+  }())
+})(jQuery);
+    
+</script>
+
+<!-- InstanceEndEditable -->
+
+
+</body>
+<!-- InstanceEnd --></html>
