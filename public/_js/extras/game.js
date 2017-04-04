d526d8321fedc17ddd19d2204f194684515f0930
smazing <smazingwebdev@gmail.com>
2017-04-04 14:30:02 -0400

    Add files via upload

--- public/_js/extras/game.js
+++ public/_js/extras/game.js
@@ -2,7 +2,9 @@
 
 var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, 'play','',true)
 
+var socket // Socket connection
 var player
+var enemies
 var cursor
 var last_clicked
 var randomX = 0;
@@ -21,6 +23,8 @@ var scaleRatio = dpr / 3
 var manager = null
 var emitter = null
 var currentSpeed = 0
+var playerCount = 0
+var username = 'John Smith'
 
 
 game.state.add('boot',bootState);
--- public/_js/extras/menu.js
+++ public/_js/extras/menu.js
@@ -1,15 +1,15 @@
 var menuState = {
 
 	create: function(){
-
+		socket = io.connect()
 		// Our tiled scrolling background
 	    // space = game.add.tileSprite(0, 0, 4000, 4000, 'space');
 	},
 
 	update: function(){
-		if(game.input.activePointer.justPressed()) {
+		//if(game.input.activePointer.justPressed()) {
 	 		game.state.start('play');
-	 	}
+	 	//}
 	}
 
 };
