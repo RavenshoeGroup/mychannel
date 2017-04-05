9d9a6b0dc741facf988c2a3a271fae61c88dc4bf
smazing <smazingwebdev@gmail.com>
2017-04-05 09:56:59 -0400

    Add files via upload

--- public/_js/extras/load.js
+++ public/_js/extras/load.js
@@ -5,6 +5,7 @@ var loadState = {
 		//var loadingLabel = game.add.text(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio,'loading...',{font:'30px Arial',fill:'#ffffff'});
 		game.load.atlas('arcade', '_js/extras/assets/arcade-joystick.png', '_js/extras/assets/arcade-joystick.json');
 		game.load.spritesheet('player', '_js/extras/assets/ball.png', 10, 10);
+		game.load.spritesheet('empty', '_js/extras/assets/1x1.png', 1, 1);
 
 	},
 
